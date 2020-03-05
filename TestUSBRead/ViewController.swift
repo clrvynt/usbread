ceae43e60db7a93cab195197bbf7dc2b9078d383
kal <clrvynt@gmail.com>
2020-03-04 20:06:38 -0500

    Initial Commit

---
+++ TestUSBRead/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestUSBRead
+//
+//  Created by kalyankrishnamurthi on 3/4/20.
+//  Copyright © 2020 KK. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
