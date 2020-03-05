a250081c16e73fe9a42fdc7be30da1c66c5ac500
kal <clrvynt@gmail.com>
2020-03-04 20:12:51 -0500

    first

--- TestUSBRead/ViewController.swift
+++ TestUSBRead/ViewController.swift
@@ -7,14 +7,62 @@
 //
 
 import UIKit
+import MobileCoreServices
 
-class ViewController: UIViewController {
+
+class ViewController: UIViewController, UIDocumentPickerDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        let documentPicker = UIDocumentPickerViewController(documentTypes: [kUTTypeFolder as String], in: .open)
+        documentPicker.delegate = self
+        documentPicker.shouldShowFileExtensions = true
+        present(documentPicker, animated: true, completion: nil)
+    }
+    
+    func documentPickerWasCancelled(_ controller: UIDocumentPickerViewController) {
+        print("Cancelled")
+    }
+    
+    func documentPicker(_ controller: UIDocumentPickerViewController, didPickDocumentsAt urls: [URL]) {
+        print("didPickDocuments at \(urls)")
+        let pickedFolderURL = urls[0]
+        // Reading the Content of a Picked Folder
+        let shouldStopAccessing = pickedFolderURL.startAccessingSecurityScopedResource()
+        defer {
+            if shouldStopAccessing {
+                pickedFolderURL.stopAccessingSecurityScopedResource()
+            }
+        }
+        var coordinatedError:NSError?
+        NSFileCoordinator().coordinate(readingItemAt: pickedFolderURL, error: &coordinatedError) { (folderURL) in
+            let keys : [URLResourceKey] = [.nameKey, .isDirectoryKey, .isReadableKey, .fileSecurityKey, .fileProtectionKey]
+            let fileList = FileManager.default.enumerator(at: pickedFolderURL, includingPropertiesForKeys: keys)!
+            for case let file as URL in fileList {
+                print(file)
+                do { try print(file.resourceValues(forKeys: Set(keys))) } catch { print(error) }
+                if !file.hasDirectoryPath {
+                    print(file)
+                    guard file.startAccessingSecurityScopedResource() else {
+                        // Handle the failure here.
+                        print("THIS ALWAYS FAILS!")
+                        continue
+                    }
+                    // Make sure you release the security-scoped resource when you are done.
+                    defer { file.stopAccessingSecurityScopedResource() }
+                    
+                    // I can read the file
+                    
+                }
+            }
+        }
+    }
 
 }
 
